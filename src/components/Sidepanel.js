3c2ae02490f51511a5b055c7a9d5235dc5b9628a
Kartik182 <91744183+Kartik182@users.noreply.github.com>
2024-11-11 13:11:36 +0530

    Update Sidepanel.js

--- src/components/Sidepanel.js
+++ src/components/Sidepanel.js
@@ -1,25 +1,61 @@
-import React from 'react'
-import './Sidepanel.css'
+import React from "react";
+import "./Sidepanel.css";
+import { TfiDashboard } from "react-icons/tfi";
+import { LuLayout } from "react-icons/lu";
+import { MdOutlineMail } from "react-icons/md";
+import { MdOutlineCalendarMonth } from "react-icons/md";
+import { MdOutlineBuild } from "react-icons/md";
+import { SiGoogleforms } from "react-icons/si";
+import { BiFileBlank } from "react-icons/bi";
+import { VscListFlat } from "react-icons/vsc";
+import { FaChartLine } from "react-icons/fa";
+import { TbMapSearch } from "react-icons/tb";
+import { CgProfile } from "react-icons/cg";
 
 function Sidepanel() {
   return (
-    <div className='sidepanel'>
-        <h1 className='main-heading'>Power Analytics</h1>
-        <div className='sidepanel-links'>
-            <p>Dashboard</p>
-            <p>Layouts</p>
-            <p>Mailbox</p>
-            <p>Calendar</p>
-            <p>UI Demands</p>
-            <p>Forms</p>
-            <p>Pages</p>
-            <p>Toolbox</p>
-            <p>Charts</p>
-            <p>Maps</p>
-            <p>Profile</p>
-        </div>
+    <div className="sidepanel">
+      <h1 className="main-heading">Power Analytics</h1>
+      <div className="sidepanel-links">
+        <p>
+          <TfiDashboard /> &nbsp; Dashboard
+        </p>
+        <p>
+          <LuLayout /> &nbsp; Layouts
+        </p>
+        <p>
+          <MdOutlineMail /> &nbsp; Mailbox
+        </p>
+        <p>
+          <MdOutlineCalendarMonth /> &nbsp; Calendar
+        </p>
+        <p>
+          <MdOutlineBuild /> &nbsp; UI Demands
+        </p>
+        <p>
+          <SiGoogleforms /> &nbsp; Forms
+        </p>
+        <p>
+          <BiFileBlank /> &nbsp; Pages
+        </p>
+        <p>
+          <VscListFlat /> &nbsp; Toolbox
+        </p>
+        <p>
+          <FaChartLine /> &nbsp; Charts
+        </p>
+        <p>
+          <TbMapSearch /> &nbsp; Maps
+        </p>
+        <p>
+          <CgProfile /> &nbsp; Profile
+        </p>
+      </div>
     </div>
-  )
+  );
+}
+
+export default Sidepanel;
 }
 
 export default Sidepanel
